f60795302c1abc9da8d433b337ff61a2ac5b45c7
Omar Nabi <onabi20@gmail.com>
2020-04-09 05:52:21 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
-# code here!
+class School
+	attr_accessor :roster, :name, :grade 
+	attr_reader :school 
+	
+	def initialize(school)
+    @school = school
+  end
+  
+  def roster
+  	roster = {}
+  end 
+  
+  def add_student(name, grade)
+    self.roster[grade] ||= []
+    self.roster[grade] << name
+  end
+
+end
